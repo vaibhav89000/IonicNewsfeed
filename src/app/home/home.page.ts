0564d7d00bb2e14492baf7bd973825e53fda6737
vaibhav89000 <vaibhav89000@gmail.com>
2021-02-07 20:30:30 +0530

    get news service working

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { NewsServiceService } from '../news-service.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
-  constructor() {}
+  constructor(private newsService:NewsServiceService) {}
+
+  ngOnInit(){
+    this.loadNews();
+  }
+
+  loadNews(){
+    this.newsService.getNews().subscribe((res)=>{
+      console.log('res',res);
+    },(err)=>{
+      console.log('err',err);
+    })
+  }
 
 }
---
+++ src/app/news-service.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../environments/environment';
+
+const API_KEY=environment.API_KEY;
+const API_URL=environment.API_URL;
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsServiceService {
+
+
+  constructor(private http:HttpClient) { }
+
+  getNews(){
+    return this.http.get(`${API_URL}top-headlines?country=in&apiKey=${API_KEY}`)
+  }
+}
